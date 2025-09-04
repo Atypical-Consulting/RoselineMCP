4daab6ef8867fac7c19d4f263be94802dfc974bb
Philippe Matray <phmatray@gmail.com>
2025-09-04 17:47:05 +0200

    Add initial project files and implement monkey service with echo tools

---
+++ MyFirstMCP/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using MyFirstMCP;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<MonkeyService>();
+
+await builder.Build().RunAsync();
